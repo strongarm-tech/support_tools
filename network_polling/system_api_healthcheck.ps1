28ed76f2ad062adf8eb05da5af5e3a1eeacf849d
Bryant Eadon <bryant@strongarmtech.com>
2021-04-27 02:01:20 -0400

    04-27-2021 02:01 BE 1. prevented cache 2. prevented DNS 3. prevented TCP socket reuse 4. prevented timeout problems 5. trace errors 6. print raw responses, including AWS trace headers

--- network_polling/system_api_healthcheck.ps1
+++ network_polling/system_api_healthcheck.ps1
@@ -1,7 +1,40 @@
 # Powershell Script for simple polling.
 
+# If the execution policy prevents this script from running,
+# please use the following command to temporarily disable the 
+# Restricted policy for only this user :
+# Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser
+# When complete, undo this by seting the default :
+# Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser
+
+# Set DNS so that the cache always expires immediately
+[System.Net.ServicePointManager]::DnsRefreshTimeout = 0
+
+
 while( 1 )
 {
-    Write-Output "Hello World!";
+    ipconfig /flushdns
+
+    $webResponse = $null;
+    $timestamp = Get-Date -Format o;
+    Write-Output ($timestamp + ": Start Request to StrongArm Tech HTTPS ping");
+
+    try {
+        $webResponse = ( Invoke-WebRequest 'https://dock.strongarmtech.com/prod/ping' -Headers @{"Cache-Control"="no-cache"} -DisableKeepAlive -TimeoutSec 60 -UseBasicParsing -Verbose );
+        # This will only execute if the Invoke-WebRequest is successful.
+        $StatusCode = $webResponse.StatusCode
+        $webResponse.Headers | Where-Object {$_.Keys -eq 'State'}
+    }
+    catch {
+        $StatusCode = $_.Exception.Response.StatusCode.value__;
+    }
+    $timestampPrev = $timestamp;
+    $timestamp = Get-Date -Format o;
+    $webResponse.InputFields 
+    Write-Output ($timestamp + ": HTTP Response Code:'" + $StatusCode + "' Response:'" + $webResponse +"' RequestTime:'" + (New-TimeSpan -End $timestamp -Start $timestampPrev ) +"'" );
+    Write-Output ($timestamp + ': RAWCONTENT:' + ( $webResponse | Select-Object -Expand RawContent) );
+
+    $timestamp = Get-Date -Format o;
+    Write-Output ($timestamp + ": Request completed");
     Start-Sleep 2;
 }
