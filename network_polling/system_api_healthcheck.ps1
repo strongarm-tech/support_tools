0964d66cba7dae262c164c1e8f690a4395f79b5e
Bryant Eadon <bryant@strongarmtech.com>
2021-04-27 23:30:14 -0400

    04-27-2021 23:30 BE 1. added DNS endpoint checks for 2x nameservers

--- network_polling/system_api_healthcheck.ps1
+++ network_polling/system_api_healthcheck.ps1
@@ -1,15 +1,29 @@
-# Powershell Script for simple polling.
+# Powershell Script for simple polling of network endpoints.
 
+# Instructions:
+#
 # If the execution policy prevents this script from running,
 # please use the following command to temporarily disable the 
 # Restricted policy for only this user :
 # Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser
 # When complete, undo this by seting the default :
 # Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser
-
+#
+#  if the script fails to run, Review the Local Poliy Editor to enable
+#  Computer Configuration -> Policies -> Administrative Templates -> Windows Components -> Windows PowerShell -> "Turn on Script Execution" = Disabled
+# Confirm the setting by starting PowerShell and running Get-ExecutionPolicy , the result should be "Unrestricted"
+#
+# Running this script :  powershell .\system_api_healthcheck.ps1 > log.txt
+#
+# Press Ctrl+C to stop script.
+#
+# Review log.txt with an editor when collection complete.
+#
 # Set DNS so that the cache always expires immediately
 [System.Net.ServicePointManager]::DnsRefreshTimeout = 0
 
+# Google Public Nameservers
+$publicDNSServer = @('8.8.8.8','8.8.4.4')
 
 function testPing {
 
@@ -72,6 +86,18 @@ while( 1 )
 {
     testPing;
     test204;
+    $timestamp = Get-Date -Format o;
+    Write-Output ($timestamp + ": Flushing local DNS Cache")
+    ipconfig /flushdns
+    $timestamp = Get-Date -Format o;
+    Write-Output ($timestamp + ": Resolving dock.strongarmtech.com with DHCP provided nameserver")
+    Resolve-DnsName dock.strongarmtech.com
+
+    Write-Output ($timestamp + ": Flushing local DNS Cache")
+    ipconfig /flushdns
+    $timestamp = Get-Date -Format o;
+    Write-Output ($timestamp + ": Resolving dock.strongarmtech.com with Alternate nameserver, 8.8.8.8")
+    Resolve-DnsName dock.strongarmtech.com -Server $publicDNSServer
 
     Start-Sleep 10;
 }
